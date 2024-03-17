23e3cc7e3c00b32c4bcbc86dbc0c2a836512e33e
refersonprado <prado.referson@gmail.com>
2024-03-17 12:34:25 -0300

    feat: adding message to errors

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,64 +3,71 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
-use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support) {
-        $supports = $support->all();
+    public function index(Support $support)
+    {
+        $supports = $support->orderBy('created_at', 'desc')->get();
         return view('admin/supports/index', compact('supports'));
     }
 
-    public function show(string|int $id) {
-        if(!$support = Support::find($id)) {
+    public function show(string | int $id)
+    {
+        if (!$support = Support::find($id)) {
             return back();
         }
 
         return view('admin/supports/show', compact('support'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('admin/supports/create');
     }
 
-    public function edit(Support $support, string|int $id) {
-        if(!$support = $support->where('id', $id)->first()) {
+    public function store(StoreUpdateSupport $request, Support $support)
+    {
+        $data = $request->all();
+        $data['status'] = 'a';
+
+        $support = $support->create($data);
+
+        return redirect()->route('supports');
+    }
+
+    public function edit(Support $support, string | int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
             return back();
         }
 
         return view('admin/supports/edit', compact('support'));
     }
 
-    public function update(Request $request, Support $support, string|int $id) {
-        if(!$support = $support->where('id', $id)->first()) {
+    public function update(StoreUpdateSupport $request, Support $support, string | int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
             return back();
         }
 
         $support->update($request->only([
             'subject',
-            'body'
+            'body',
         ]));
 
         return redirect()->route('supports');
     }
 
-    public function destroy(Support $support, string|int $id) {
-        if(!$support = $support->where('id', $id)->first()) {
+    public function destroy(Support $support, string | int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
             return back();
         }
 
         $support->delete();
-        return redirect()->route('supports');
-    }
-
-    public function store(Request $request, Support $support) {
-        $data = $request->all();
-        $data['status'] = 'a';
-        
-        $support = $support->create($data);
-
         return redirect()->route('supports');
     }
 }
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,9 +1,15 @@
 <h1>Duvida {{ $support->id }}</h1>
 
+@if($errors->any())
+  	@foreach($errors->all() as $error)
+    	{{$error}}
+  	@endforeach
+@endif
+
 <form action="{{route('supports/update', $support->id)}}" method="POST">
-  @csrf
-  @method('PUT')
-  <input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
-  <textarea name="body" cols="30" rows="5" placeholder="Descrição" name="body">{{ $support->body }}</textarea>
-  <button type="submit">Enviar</button>
+  	@csrf
+  	@method('PUT')
+	<input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
+  	<textarea name="body" cols="30" rows="5" placeholder="Descrição" name="body">{{ $support->body }}</textarea>
+  	<button type="submit">Enviar</button>
 </form>
